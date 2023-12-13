aa1ba80881e42fc922650097cbb170728d6c8a27
lhj8390 <lhj8390@naver.com>
2023-12-13 23:43:37 +0900

    gorm close 설명 추가

--- gorm/main.go
+++ gorm/main.go
@@ -8,9 +8,8 @@ import (
 )
 
 type Book struct {
-	gorm.Model
-	ID   int `gorm:"primaryKey"`
-	Name string
+	gorm.Model // ID, CreatedAt, UpdatedAt, DeletedAt 필드를 포함하는 구조체
+	Name       string
 }
 
 func main() {
@@ -18,13 +17,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to connect DB : %v", err)
 	}
-	// migration
-	db.AutoMigrate(&Book{})
 
-	db.Create(&Book{ID: 1, Name: "test"})
+	defer func() {
+		// 전체 코드가 완전히 실행되면 연결을 종료한다.
+		// GORM v1.20 부터는 connection pooling 을 지원하기 때문에
+		// connection 을 열고 어플리케이션 내에서 공유하는 것이 적절한 방법.
+		dbInstance, _ := db.DB()
+		_ = dbInstance.Close()
+	}()
+	// migration
+	err = db.AutoMigrate(&Book{})
+	if err != nil {
+		return
+	}
 
 	// 데이터 조회
-	var book Book
-	db.First(&book)
-	fmt.Printf("result : (%v %v)", book.ID, book.Name)
+	var books []Book
+	db.Find(&books)
+	fmt.Printf("result : (%v)", len(books))
 }
